afb622f858a8b58cfecbe7295144656c566be3d2
Aitor Couñago Figueroa <aitorcf@gmail.com>
2023-03-10 10:31:29 +0100

    Add: API fetch, button functionality and filters functionality

--- src/app/pages/home/components/filters/filters.component.ts
+++ src/app/pages/home/components/filters/filters.component.ts
@@ -1,15 +1,31 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-filters',
   templateUrl: 'filters.component.html'
 })
-export class FiltersComponent {
+export class FiltersComponent implements OnInit, OnDestroy {
   @Output() showCategory = new EventEmitter<string>();
+  categoriesSubscription: Subscription | undefined;
+  categories: Array<string> | undefined;
 
-  categories = ['shoes', 'shirts','sports'];
+  constructor(private storeService: StoreService) {}
+
+  ngOnInit(): void {
+    this.categoriesSubscription = this.storeService.getAllCategories().subscribe((response) => {
+      this.categories = response;
+    })
+  }
 
   onShowCategory(category: string): void {
     this.showCategory.emit(category);
+  }
+
+  ngOnDestroy(): void {
+    if (this.categoriesSubscription) {
+      this.categoriesSubscription.unsubscribe();
+    }
   }
 }
